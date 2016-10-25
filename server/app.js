31e86cd7b945a80320bcb48f7be56502f0f17a66
Abed Zantout <abed-zantout@live.com>
2016-10-25 22:10:16 +0300

    render angular app

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var bodyParser   = require('body-parser');
 var routes       = require('./routes/index');
 var app          = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../client/dist'));
 app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.engine('html', require('ejs').renderFile);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
