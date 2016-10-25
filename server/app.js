057d5722292c1fd9427a526c20441fe95388984e
Abed Zantout <abed-zantout@live.com>
2016-10-25 22:28:30 +0300

    removed err temp

--- server/app.js
+++ server/app.js
@@ -19,11 +19,11 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/', routes);
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err    = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function (req, res, next) {
+//     var err    = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 // error handlers
 // development error handler
 // will print stacktrace
