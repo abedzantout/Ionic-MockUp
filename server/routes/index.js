fb1389cd8d8af4c9f3043f7f4b5629668cf3add3
Abed Zantout <abed-zantout@live.com>
2016-11-03 20:25:55 +0200

    indentation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,38 +1,19 @@
 var express = require('express');
-var router = express.Router();
-
+var router  = express.Router();
 var fs = require('fs');
-
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('../../client/dist/index.html');
+    res.render('../../client/dist/index.html');
 });
-
-router.get('/getIconfig' , function(req, res, next){
-
-
-    fs.readFile('iconfig.json', 'utf8',  (err, data)=>{
-
+router.get('/getIconfig', function (req, res, next) {
+    fs.readFile('iconfig.json', 'utf8', (err, data)=> {
         res.send(data);
-
     });
-
-
 });
-
-
-router.post('/sendJson', function(req, res, next){
-
-
-    fs.writeFile('iconfig.json', JSON.stringify(req['body']), function(err){
+router.post('/sendJson', function (req, res, next) {
+    fs.writeFile('iconfig.json', JSON.stringify(req[ 'body' ]), function (err) {
         console.log(err);
-        
     });
-
-
-  res.send("Changes saved.");
-
+    res.send("Changes saved.");
 });
-
 module.exports = router;
