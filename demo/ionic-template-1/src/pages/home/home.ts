2c8972128ff9657f1bc4359024d6046e4aaebd58
Abed Zantout <abed-zantout@live.com>
2016-11-03 19:26:27 +0200

    (ionic-demo): updated application

---
+++ demo/ionic-template-1/src/pages/home/home.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+
+import { MenuPage } from '../menu/menu';
+import { AboutPage } from '../about/about';
+import { FeedbackPage } from '../feedback/feedback';
+import { Service } from "../../services/service";
+
+
+@Component({
+    selector: 'page-home',
+    templateUrl: 'home.html'
+})
+export class HomePage {
+    content: Array<{ title: string, component: any }>;
+    title: string;
+    jsonContent: Object;
+
+    constructor(private navCtrl: NavController, private _service: Service) {
+
+        this.jsonContent = this._service.getJson().subscribe(
+            (data) => { this.jsonContent = data; },
+            (err) => { console.log(err); },
+            () => {
+                this._service.setJsonContent(this.jsonContent);
+                this.setJsonLocally();
+            }
+        );
+    }
+
+    private setJsonLocally() {
+        this.jsonContent = JSON.parse(this._service.getJsonContent());
+        this.title = this.jsonContent['Application']['pages'][0]['hello-ionic']['title'];
+        this.content = [
+            {
+                title: this.jsonContent['Application']['pages'][0]['hello-ionic']['content'][0]['title'],
+                component: MenuPage
+            },
+            {
+                title: this.jsonContent['Application']['pages'][0]['hello-ionic']['content'][1]['title'],
+                component: AboutUsPage
+            },
+            {
+                title: this.jsonContent['Application']['pages'][0]['hello-ionic']['content'][2]['title'],
+                component: FeedbackPage
+            }
+        ]
+
+    }
+
+    private getLocalContent() {
+        return this.jsonContent;
+    }
+
+    goToPage(page) {
+        this.navCtrl.push(page.component);
+    }
+}
---
+++ demo/ionic-template-1/src/pages/menu/menu.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { FoodItemsProvider } from '../../providers/food-items-provider';
+import { foodItem } from '../foodItem';
+import { Service } from "../../services/service";
+
+/*
+ Generated class for the Menu page.
+
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
+
+
+@Component({
+    selector: 'page-menu',
+    templateUrl: 'menu.html',
+    providers: [FoodItemsProvider]
+})
+
+export class MenuPage {
+
+    title: string;
+    content: Object;
+    private jsonContent: Object;
+
+    constructor(private navCtrl: NavController, private _service: Service) {
+
+        this.jsonContent = this._service.getJson().subscribe(
+            (data) => { this.jsonContent = data; },
+            (err) => { console.log(err); },
+            () => {
+                this._service.setJsonContent(this.jsonContent);
+                this.setJsonLocally();
+            }
+        );
+    }
+
+    private setJsonLocally() {
+        this.jsonContent = JSON.parse(this._service.getJsonContent());
+        let content = this.jsonContent['Application']['pages'][2]['menu'];
+        this.title = content['title'];
+        this.content = content['content'];
+    }
+
+
+}
