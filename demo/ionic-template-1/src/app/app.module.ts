a81311e8e1f83404b4e1c60841c7445307e40888
Abed Zantout <abed-zantout@live.com>
2016-11-11 22:09:29 +0200

    added missing files

---
+++ demo/ionic-template-1/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { MenuPage } from '../pages/menu/menu';
+import { AboutPage } from '../pages/about/about';
+import { FeedbackPage } from '../pages/feedback/feedback';
+import { Service } from "../services/service";
+
+@NgModule({
+    declarations: [
+        MyApp,
+        HomePage,
+        MenuPage,
+        AboutPage,
+        FeedbackPage
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [ IonicApp ],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        MenuPage,
+        AboutPage,
+        FeedbackPage
+    ],
+    providers: [ Service ]
+})
+export class AppModule {
+}
