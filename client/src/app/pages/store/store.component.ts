c77f4c891c0c304899f1e384e9e732e389dda301
Abed Zantout <aaz15@mail.aub.edu>
2016-11-27 19:24:04 +0200

    (store):  updated ionic apps with unique title, description and image

--- client/src/app/pages/store/store.component.ts
+++ client/src/app/pages/store/store.component.ts
@@ -8,13 +8,30 @@ declare const require: any;
 })
 export class StoreComponent {
 
-    private countCards: Array<string>;
+    private countCards: Array<Object>;
 
     constructor() {
-        this.countCards = [ 'assets/app-icons/classic-diner-icon.jpg',
-            'assets/app-icons/conference-icon.jpg',
-            'assets/app-icons/mean-critique-icon.jpg',
-            'assets/app-icons/Ticket-Vender-icon.jpg' ]
+        this.countCards = [ {
+            "image": 'assets/app-icons/classic-diner-icon.jpg',
+            "title": "Classic Diner",
+            "description": "Restaurant menu application"
+        },
+            {
+                "image": 'assets/app-icons/conference-icon.jpg',
+                "title": "Conference",
+                "description": "Conference event scheduling"
+            },
+
+            {
+                "image": 'assets/app-icons/mean-critique-icon.jpg',
+                "title": "Mean Critique",
+                "description": "movie review application"
+            },
+            {
+                "image": 'assets/app-icons/Ticket-Vender-icon.jpg',
+                "title": "Ticket Vender",
+                "description": "selling event tickets"
+            } ]
     }
 
 }
