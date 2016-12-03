7c7ef4628437cf5ce106e84b86a50fbd7e59c896
Abed Zantout <aaz15@mail.aub.edu>
2016-12-03 23:42:39 +0200

    (uploader): implemented ability to add a new folder with current date TODO: implementation by id

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -4,12 +4,17 @@ const multer  = require("multer");
 const crypto  = require('crypto');
 const path    = require('path');
 const storage = multer.diskStorage({
-    destination: '../uploads/',
+    destination: (req, file, cb) => {
+        now = Date.now().toString();
+        require('fs').mkdir('../ionic-templates/' + now, err => {
+            cb(null, '../ionic-templates/' + now);
+        });
+    },
     filename   : (req, file, cb) => {
         cb(null, file.originalname.split('/').pop().trim());
     }
 });
-let upload    = multer({dest: '../uploads', storage: storage});
+let upload    = multer({dest: '../ionic-templates', storage: storage});
 router.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
